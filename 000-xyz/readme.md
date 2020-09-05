12ba89aed0ddde55fef261f0f02242967af0cf9d
xgqfrms <xgqfrms@outlook.com>
2020-09-05 23:57:37 +0800

    jest update

--- 000-xyz/readme.md
+++ 000-xyz/readme.md
@@ -8,3 +8,13 @@ $ yarn add -D jest
 
 
 ```
+
+## babel
+
+```sh
+$ yarn add -D babel-jest @babel/core @babel/preset-env
+
+$ yarn add -D babel-plugin-dynamic-import-node
+```
+
+```
---
+++ src/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-07-01
+ * @modified
+ *
+ * @description Learning JavaScript with MDN
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ */
+
+const log = console.log;
+
+const App = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+
+// export default App;
+
+// export {
+//   App,
+// };
