eda140ef4eb7efd7298519e8e977bb9051c37811
xgqfrms <xgqfrms@outlook.com>
2020-11-19 22:51:20 +0800

    Create for-each...in(removed).js

---
+++ javascript/references/statements-and-declarations/for-each...in(removed).js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-01-01
+ * @modified
+ *
+ * @description
+ * @augments
+ * @example
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for-each...in
+ * @link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/for-each...in
+ *
+ */
+
+const log = console.log;
+
+// 已废弃 👎 ❌
