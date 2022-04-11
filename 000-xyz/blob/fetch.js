908aeabe83247a677897c06df89689507b42b24e
xgqfrms <xgqfrms@outlook.com>
2022-04-12 02:43:30 +0800

    不仅支持 XMLHttpRequest ✅ & 还支持 Fetch API ✅

--- 000-xyz/blob/fetch.js
+++ 000-xyz/blob/fetch.js
@@ -1,7 +1,28 @@
-async function generatorBlobVideo(url, type, dom, link) {
+
+async function recover (link, blob) {
+  // 还原 blob
+  const text = await (new Response(blob)).text();
+  console.log('text =', text);
+  link.innerText = text;
+  // auto click
+  link.click();
+  URL.revokeObjectURL(link.href);
+  //
+  try {
+    const json = await (new Response(blob)).json();
+    console.log('json =', json);
+  } catch(err) {
+    console.log('err =', err);
+  }
+}
+
+async function generatorBlobVideo(url, type, dom, link, pre) {
   const headers = {
     responseType: 'arraybuffer',
   };
+  // if(!type.includes('json')) {
+  //   headers.responseType = 'arraybuffer';
+  // }
   // const headers = new Headers({
   //   'Content-Type': 'text/plain',
   //   'X-Custom-Header': 'ProcessThisImmediately',
@@ -22,32 +43,95 @@ async function generatorBlobVideo(url, type, dom, link) {
     // credentials: 'same-origin',
     // credentials: 'include',
     method: 'GET',
-    mode: 'no-cors',
-    responseType: 'arraybuffer',
+    // mode: 'no-cors',
+    // ❌  A no-cors request makes the response type opaque. Opaque means your frontend JavaScript code can’t see the response body or headers.
+    // responseType: 'arraybuffer',
+    // responseType: 'blob',
+    ...headers,
     headers: {
+      // 'Accept': type,
+      'Content-Type': type,
+      // 'Content-Type': 'application/octet-stream',
       // 'Accept': 'video/mp4',
       // 'Accept': 'application/json',
-      'Accept': type,
-      'Content-Type': type,
       // 'Content-Type': 'application/json;charset=utf-8'
       // 'Content-Type': 'application/json',
       // 'Content-Type': 'application/x-www-form-urlencoded',
       // ...headers,
     },
   }).then(res => {
-    console.log('fetch.response =', res);
-    const buffer = res.arrayBuffer();
+    console.log('res =', res);
+    if(type.includes('json')) {
+      const json = res.json();
+      console.log('res.json =', json);
+      pre.innerText = JSON.stringify(json, null, 4);
+      // return json;
+    } else {
+      // const buffer = res.arrayBuffer();
+      // console.log('buffer =', buffer);
+      // return buffer;
+    }
+    return res.arrayBuffer();
+  }).then(data => {
+    console.log('data =', data);
     const blob = new Blob(
-      [buffer],
+      [data],
       {'type' : type},
     );
-    // const file = new File(buffer, 'test');
     const urlBlob = URL.createObjectURL(blob);
-    dom.src = urlBlob;
+    if(!type.includes('json')) {
+      dom.src = urlBlob;
+    } else {
+      dom.dataset.src = urlBlob;
+    }
+    // dom.src = urlBlob;
     link.href = urlBlob;
-    link.innerText = urlBlob;
+    console.log(`type.includes('json') =`, type.includes('json'))
+    if(type.includes('json')) {
+      console.log('recover ...');
+      recover(link, blob);
+    } else {
+      console.log('urlBlob =', urlBlob);
+      link.innerText = urlBlob;
+    }
   }).catch(err => {}).finally(() => {});
+  // .then(res => {
+  //   // console.log('fetch.response =', res);
+  //   const json = res.json();
+  //   console.log('json =', json);
+  //   const buffer = res.arrayBuffer();
+  //   console.log('buffer =', buffer);
+  //   // const buffer = res.blob();
+  //   const blob = new Blob(
+  //     [buffer],
+  //     {'type' : type},
+  //   );
+  //   // const file = new File(buffer, 'test');
+  //   const urlBlob = URL.createObjectURL(blob);
+  //   dom.src = urlBlob;
+  //   link.href = urlBlob;
+  //   link.innerText = urlBlob;
+  // }).catch(err => {}).finally(() => {});
+  // url !== file
+  // const blob = new Blob(
+  //   [url],
+  //   {'type' : type},
+  // );
+  // // const file = new File(buffer, 'test');
+  // const urlBlob = URL.createObjectURL(blob);
+  // dom.src = urlBlob;
+  // link.href = urlBlob;
+  // link.innerText = urlBlob;
 }
+(async function() {
+  var type = 'application/json';
+  var url = 'https://cdn.xgqfrms.xyz/json/honor.json';
+  var dom = document.querySelector('#json');
+  var link = document.querySelector('#json-link');
+  var pre = document.querySelector('#json-pre');
+  // console.log('img link =', link);
+  await generatorBlobVideo(url, type, dom, link, pre);
+})();
 (async function() {
   var type = 'image/png';
   var url = 'https://cdn.xgqfrms.xyz/logo/icon.png';
